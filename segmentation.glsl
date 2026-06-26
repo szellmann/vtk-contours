be01f940ca7b84149351e4fe6c4d293fa66aa889
Stefan Zellmann <info@szellmann.de>
2026-06-26 02:13:36 +0200

    Start porting LocateSuperarcs worklet to FS

--- segmentation.glsl
+++ segmentation.glsl
@@ -15,6 +15,17 @@ uniform usampler2D hyperparents;
 uniform usampler2D whenTransferred;
 uniform usampler2D hyperarcs;
 
+// ./filter/scalar_topology/worklet/contourtree_augmented//Types.h
+#define NO_SUCH_ELEMENT 0x8000000u
+//constexpr viskores::Id NO_SUCH_ELEMENT = std::numeric_limits<viskores::Id>::min();
+//constexpr viskores::Id TERMINAL_ELEMENT = std::numeric_limits<viskores::Id>::max() / 2 + 1; //0x40000000 || 0x4000000000000000
+//constexpr viskores::Id IS_SUPERNODE = std::numeric_limits<viskores::Id>::max() / 4 + 1; //0x20000000 || 0x2000000000000000
+//constexpr viskores::Id IS_HYPERNODE = std::numeric_limits<viskores::Id>::max() / 8 + 1; //0x10000000 || 0x1000000000000000
+//constexpr viskores::Id IS_ASCENDING = std::numeric_limits<viskores::Id>::max() / 16 + 1; //0x08000000 || 0x0800000000000000
+//constexpr viskores::Id INDEX_MASK = std::numeric_limits<viskores::Id>::max() / 16; //0x07FFFFFF || 0x07FFFFFFFFFFFFFF
+//constexpr viskores::Id CV_OTHER_FLAG = std::numeric_limits<viskores::Id>::max() / 8 + 1; //0x10000000 || 0x1000000000000000
+//constexpr viskores::Id ELEMENT_EXISTS = std::numeric_limits<viskores::Id>::max() / 4 + 1; //0x20000000 || 0x2000000000000000 , same as IS_SUPERNODE
+
 vec3 randomColor(uint idx) {
   uint r = uint(int(idx)*13*17 + 0x234235);
   uint g = uint(int(idx)*7*3*5 + 0x773477);
@@ -30,13 +41,12 @@ struct ID_t {
 };
 
 // 32-bit index from 64-bit ID
-uint getIndex(ID_t id) {
-  return id.lo;
+uint maskedIndex(ID_t id) {
+  return id.lo; // TODO..
 }
 
-// upper 5 bits contain flags
-uint getFlags(ID_t id) {
-  return (id.hi >> 27u) & 0x1Fu;
+bool noSuchElement(ID_t id) {
+  return (id.hi & NO_SUCH_ELEMENT) != 0u;
 }
 
 struct ui64x2_t {
@@ -53,6 +63,14 @@ ID_t getID(usampler2D samp, int index) {
     return ID_t(ui4.y,ui4.w);
 }
 
+ID_t getID(usampler2D samp, uint index) {
+  return getID(samp, int(index));
+}
+
+ID_t getID(usampler2D samp, ID_t id) {
+  return getID(samp, int(id.lo));
+}
+
 float getData(int nodeID) {
   int i=nodeID%4096;
   int j=nodeID/4096;
@@ -64,6 +82,7 @@ void main() {
   vec4 texColor = texture(actortexture, tcoordVCVSOutput);
   gl_FragData[0] = texColor;
 
+  // compute the triangle we're in
   vec2 uv = tcoordVCVSOutput.xy;
   int x = int(uv.x*float(dims.x-1));
   int y = int(uv.y*float(dims.y-1));
@@ -82,36 +101,54 @@ void main() {
 
   float dat0[3]; dat0[0]=getData(p0); dat0[1]=getData(p1); dat0[2]=getData(p2);
   float dat1[3]; dat1[0]=getData(p0); dat1[1]=getData(p2); dat1[2]=getData(p3);
-  int vlo=-1, vhi=-1;
+  int bottom=-1, top=-1;
   float minValue=1e31f, maxValue=-1e31f;
+
+  // in the bottom/right triangle, u-coord "grows" faster,
+  // in the top/left triangle, v-coord "grows" faster:
   int triID = (xfrac >= yfrac) ? 0 : 1;
 
+  // compute top and bottom ID (triangle vertices with max and min value):
   for (int i=0; i<3; ++i) {
     if (triID==0) {
       if (dat0[i] < minValue) {
         minValue = dat0[i];
-        vlo = tri0[i];
+        bottom = tri0[i];
       }
 
       if (dat0[2-i] > maxValue) {
         maxValue = dat0[2-i];
-        vhi = tri0[2-i];
+        top = tri0[2-i];
       }
     } else {
       if (dat1[i] < minValue) {
         minValue = dat1[i];
-        vlo = tri1[i];
+        bottom = tri1[i];
       }
 
       if (dat1[2-i] > maxValue) {
         maxValue = dat1[2-i];
-        vhi = tri1[2-i];
+        top = tri1[2-i];
       }
     }
   }
 
-  uint SP = getIndex(getID(superparents, vhi));
-  gl_FragData[0] = vec4(randomColor(SP), 1.f);
+  // BEGIN LocateSuperarcs
+  if (true) {
+    // regular nodes only
+    // we will need to prune top and bottom until one of them prunes past the node
+    ID_t topSuperparent = getID(superparents, top);
+    ID_t bottomSuperparent = getID(superparents, bottom);
+    // and we can also find out when they transferred
+    ID_t topWhen = getID(whenTransferred, topSuperparent);
+    ID_t bottomWhen = getID(whenTransferred, bottomSuperparent);
+    // and their hyperparent
+    ID_t topHyperparent = getID(hyperparents, topSuperparent);
+    ID_t bottomHyperparent = getID(hyperparents, bottomSuperparent);
+
+    gl_FragData[0] = vec4(randomColor(maskedIndex(bottomHyperparent)), 1.f);
+  }
+
 }
 
 
